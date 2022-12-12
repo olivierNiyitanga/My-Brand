663999c288c53c989aa611a20230464564c622c7
KABANOFESTO <kabanofesto1@gmail.com>
2022-12-13 01:55:16 +0200

    login correction

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function signUp() {
+function signUp(e){
+    event.preventDefault();
     var email = document.getElementById('email').value;
     var flname = document.getElementById('flname').value;
     var pwd = document.getElementById('pwd').value;
@@ -9,27 +10,24 @@ function signUp() {
         pwd: pwd,
     };
     var json = JSON.stringify(formData);
-    localStorage.setItem('user', json);
-    console.log('well done');
-    return false
+    localStorage.setItem(flname, json);
+    alert('well done');
 }
-function loginfunc() {
-
+function loginfunc(e) {
+event.preventDefault();
     var flname = document.getElementById('flname').value;
     var pwd = document.getElementById('pwd').value;
     var result = document.getElementById('result');
 
-    var name = localStorage.getItem(flname);
+    var name = localStorage.getItem('flname');
     var data = JSON.parse(name);
-    console.log(data);
     if (name == null) {
-        window.location.href = "dashBoard.html";
+        result.innerHTML= "correct your username plz!!!";
     }
     else if (flname == data.flname && pwd == data.pwd) {
         window.location.href = "dashBoard.html";
     }
     else {
-        result.innerHTML = "wrong password";
+        result.innerHTML = "correct your password plz!!!";
     }
-    return false
 }
